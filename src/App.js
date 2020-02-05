4b71c45bbd86a51d272b6728139475978f6b18ce
Sam Wilkinson <samwilkinson.github@gmail.com>
2020-02-05 12:31:33 +0000

    reformated data, moved conditional highlight to be a function (not directly in JSX)

--- src/App.js
+++ src/App.js
@@ -4,59 +4,66 @@ import Table from './Table.js'
 
 function App() {
   
+  //Set initial state data
+  //To mimic how a csv file might be destructured (Header and subsequent rows)
   const [tableData, setTableData] = useState({
-    columnNames : ['name', 'col2', 'col3', 'col4', 'col5'],
-    tableEntries : [
-      {
-        name : 'name1',
-        col2 : 'entry2',
-        col3 : 'entry3',
-        col4 : 'entry4',
-        col5 : 'entry5',
-      },
-      {
-        name : 'name1',
-        col2 : 'entry2',
-        col3 : 'entry3',
-        col4 : 'entry4',
-        col5 : 'entry5',
-      },
-      {
-        name : 'name1',
-        col2 : 'entry2',
-        col3 : 'entry3',
-        col4 : 'entry4',
-        col5 : 'entry5',
-      },
-      {
-        name : 'name1',
-        col2 : 'entry2',
-        col3 : 'entry3',
-        col4 : 'entry4',
-        col5 : 'entry5',
-      },{
-        name : 'name1',
-        col2 : 'entry2',
-        col3 : 'entry3',
-        col4 : 'entry4',
-        col5 : 'entry5',
-      }
+    tableHeader : ['id', 'col2', 'col3', 'col4', 'col5'],
+    tableRows : [
+       [
+        'name1',
+        'entry2entry2entry2entry2entry2entry2entry2entry2',
+        'entry3',
+        'entry4',
+        'entry5'
+       ],
+       [
+        'name1',
+        'entry2entry2entry2entry2entry2entry2entry2entry2',
+        'entry3',
+        'entry4',
+        'entry5'
+       ],
+       [
+        'name1',
+        'entry2entry2entry2entry2entry2entry2entry2entry2',
+        'entry3',
+        'entry4',
+        'entry5'
+       ],
+       [
+        'name1',
+        'entry2entry2entry2entry2entry2entry2entry2entry2',
+        'entry3',
+        'entry4',
+        'entry5'
+       ],
+       [
+        'name1',
+        'entry2entry2entry2entry2entry2entry2entry2entry2',
+        'entry3',
+        'entry4',
+        'entry5'
+       ]
     ]
   })
 
+  //table visibility
   const [tableVisible, setTableVisible] = useState(true)
 
   return (
     <div className="App">
       <div className = 'App-link' >
-      
+        {/* When clicked will hide/unhide table*/}
         <span onClick = {() => setTableVisible(!tableVisible)}>
           Toggle Table
         </span>
       </div>
+
+      {/*Conditional render for show table from state*/}
       {tableVisible &&
         <Table tableData = {tableData} setTableVisible = {() => setTableVisible(!tableVisible)} />
       }
+
     </div>
   );
 }
--- src/Table.js
+++ src/Table.js
@@ -3,38 +3,87 @@ import './Table.css'
 
 function Table(props) {
     
-    let {columnNames, tableEntries} = props.tableData
-    const [highlightCell, setHighlightCell] = useState({ x : '' , y : '' })
+    //Props
+    let {tableHeader, tableRows} = props.tableData
 
+    //State, currently hovered table data cell
+    const [highlightedCellCoordinates, setHighlightedCellCoordinates] = useState({ x : '' , y : '' })
+
+
+    //MouseEnter/Leave table cell handlers
     let cellEnter = (x,y) => {
-        setHighlightCell({x,y})
+        setHighlightedCellCoordinates({x,y})
     }
 
     let cellLeave = () => {
-        setHighlightCell({x : '',y : ''})
+        setHighlightedCellCoordinates({x : '',y : ''})
     }
 
-    let thisCellIsHighlighted = (x, y) => {
-        return x === highlightCell.x && y === highlightCell.y
+    //returns bool describing if data cell should be highlighted
+    let highlightCell = (x, y) => {
+        //should highlight?
+        let highlight = false
+
+            /* Highlight Rules */
+            /* Left most cell highlighted on hover (row entry name/id) */
+            /* Remaining cells highlight on hover in addition to left most cell */
+            // (header highlighting is handled in render)
+
+            //highlight left most cell if on same row as hovered coords
+        if ((x === 0 && y === highlightedCellCoordinates.y) ||
+            //highlight if match hovered coords
+            (x === highlightedCellCoordinates.x && y === highlightedCellCoordinates.y)) {
+
+                highlight = true
+
+        }
+
+        //return data cell with highlight? and mouseEvents
+        return highlight
+
     }
 
+    //Return a sementic table
     return (
-    
         <table id = "table">
             <tbody>
-
-                <tr>
-                    {columnNames.map((name, index) => <th className = {highlightCell.y === index ? 'highlight' : ''} onMouseEnter = {() => cellEnter('', index)} onMouseLeave = {() => cellLeave()}>{name}</th>)}
+                {/* Table Header derived from props.tableHeader
+                    takes an array and uses Array.map to output an array containing JSX
+                 */}
+                <tr key = {0}>
+                    {tableHeader.map((name, xIndex) => {
+                        return (
+                            <th key = {xIndex} 
+                                className = {highlightedCellCoordinates.x === xIndex ? 'highlight' : ''} 
+                                onMouseEnter = {() => cellEnter(xIndex, '')} 
+                                onMouseLeave = {() => cellLeave()}>
+                                    {name}
+                            </th>)}
+                        )
+                    }
                 </tr>
 
-                {tableEntries.map(
-                    (entry, xIndex) => {
-                        let rowDataEntries = []
-                            columnNames.forEach((name, yIndex) => 
-                                rowDataEntries.push(<td className = {yIndex === 0 && highlightCell.x === xIndex || thisCellIsHighlighted(xIndex, yIndex) ? 'highlight' : ''} 
-                                    onMouseEnter = {() => cellEnter(xIndex, yIndex)} 
-                                    onMouseLeave = {() => cellLeave()}>{entry[name]}</td>))
-                            return <tr>{rowDataEntries}</tr>
+                {/* Generate Table rows containing table data
+                 */}
+                {tableRows.map(
+                    (row, yIndex) => {
+                        //generate rows JSX from props.tableRows
+                        return (
+                            <tr key = {yIndex + 1}>
+                                {/* return row JSX consisting of table data cells
+                                */}
+                                {row.map((entry, xIndex) => {
+                                    return(
+                                        <td key = {xIndex} className = {highlightCell(xIndex, yIndex) ? 'highlight' : ''}
+                                                onMouseEnter = {() => cellEnter(xIndex, yIndex)}
+                                                onMouseLeave = {() => cellLeave()}
+                                            >
+                                                {entry}
+                                        </td>
+                                    )
+                                })}
+                            </tr>
+                        )
                     }
                 )} 
 
@@ -48,3 +97,4 @@ function Table(props) {
   }
   
 export default Table
+
